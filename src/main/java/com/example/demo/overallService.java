d1482bc44727309143675e5babebffb6f0069525
Fredrikkarm <91528690+Fredrikkarm@users.noreply.github.com>
2022-03-31 22:14:41 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/overallService.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class overallService {
+
+	
+	private ArrayList<orderBean> orders;
+	
+	public void readFile(String filename) throws IOException {
+		csvFileReader cs = new csvFileReader();
+		orders = cs.readFile(filename);
+		
+	}
+	
+	public String toJson() {
+		String order = "";
+		//System.out.println(orders);
+		//System.out.println(order);
+		
+		for (orderBean orderBean : orders) {
+			order += orderBean.toJsonString() + ",";
+		}
+		if (order.length() > 2){
+			order = order.substring(0, order.length() - 1);
+		}
+		System.out.println(order);
+		order = "{ \"Customer Order\":  [" + order + "] }";
+		
+		return order;
+		
+	}
+	
+}
